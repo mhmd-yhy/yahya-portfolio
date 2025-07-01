c8023ba143b4a22f4a64de0860e721ed7220520b
Muhammed Yahya <138642434+mhmd-yhy@users.noreply.github.com>
2025-07-01 13:07:57 +0300

    Add files via upload

--- src/assets/data.js
+++ src/assets/data.js
@@ -6,13 +6,17 @@ let pathname = "";
 if (window.location.pathname === "/dashboard") pathname = "/dashboard";
 if (window.location.pathname === "/login") pathname = "/login";
 
-export const login = { email: "admin@gmail.com", password: "adminZx", icon: Icons.login.Login };
+export const login = {email: "admin@gmail.com", password: "adminZx", icon: Icons.login.Login};
 export const links = [
-  { icon: Icons.links.PersonOutline, text: "about", path: "/" },
-  { icon: Icons.links.LaptopCode, text: "services", path: "/services" },
-  { icon: Icons.links.BriefcaseOutline, text: "portfolio", path: "/portfolio" },
-  { icon: Icons.links.OutlineContacts, text: "contact", path: "/contact" },
-  (pathname === "/dashboard" || pathname === "/login") && { icon: pathname === "/dashboard" ? Icons.links.Dashboard : Icons.links.Login, text: pathname === "/dashboard" ? "dashboard" : "login", path: pathname === "/dashboard" ? "/dashboard" : "/login" },
+  {icon: Icons.links.PersonOutline, text: "about", path: "/"},
+  {icon: Icons.links.LaptopCode, text: "services", path: "/services"},
+  {icon: Icons.links.BriefcaseOutline, text: "portfolio", path: "/portfolio"},
+  {icon: Icons.links.OutlineContacts, text: "contact", path: "/contact"},
+  (pathname === "/dashboard" || pathname === "/login") && {
+    icon: pathname === "/dashboard" ? Icons.links.Dashboard : Icons.links.Login,
+    text: pathname === "/dashboard" ? "dashboard" : "login",
+    path: pathname === "/dashboard" ? "/dashboard" : "/login",
+  },
 ];
 export const landing = {
   image: Images.avatar,
@@ -23,7 +27,7 @@ export const landing = {
       icon: Icons.contactInfo.PhoneIphone,
       iconColor: "text-blue-500",
       text: "phone",
-      desc: "+90 536 675 13 48",
+      desc: "+963 951 406 315",
       borderBottom: true,
     },
     {
@@ -37,7 +41,7 @@ export const landing = {
       icon: Icons.contactInfo.LocationDot,
       iconColor: "text-blue-500",
       text: "loation",
-      desc: "Istanbul/Turkey",
+      desc: "Harasta/Damascus",
       borderBottom: true,
     },
     {
@@ -67,7 +71,7 @@ export const landing = {
     {
       icon: Icons.social.Whatsapp,
       color: "text-green-700",
-      href: "https://wa.me/905366751348",
+      href: "https://wa.me/963951406315",
     },
   ],
   showContactIcon: Icons.landing.showContactIcon,
@@ -94,9 +98,9 @@ export const about = {
     },
   ],
   stats: [
-    { icon: Icons.stats.PersonOutline, text: "clints", stat: "10" },
-    { icon: Icons.stats.Validate, text: "years experience", stat: "2" },
-    { icon: Icons.stats.LaptopCode, text: "projects", stat: "20" },
+    {icon: Icons.stats.PersonOutline, text: "clints", stat: "10"},
+    {icon: Icons.stats.Validate, text: "years experience", stat: "2"},
+    {icon: Icons.stats.LaptopCode, text: "projects", stat: "20"},
   ],
 };
 export const services = {
@@ -123,13 +127,13 @@ export const services = {
     },
   ],
   skills: [
-    { icon: Icons.skills.Html5, color: "#e6511e", title: "Html5" },
-    { icon: Icons.skills.Css3Alt, color: "#2D53E5", title: "Css3Alt" },
-    { icon: Icons.skills.JsSquare, color: "#f4e224", title: "JsSquare" },
-    { icon: Icons.skills.React, color: "#08d9ff", title: "React" },
-    { icon: Icons.skills.redux, color: "#7a50be", title: "redux" },
-    { icon: Icons.skills.Bootstrap, color: "#7719f7", title: "Bootstrap" },
-    { icon: Icons.skills.Tailwind, color: "#0eb8d5", title: "Tailwind" },
+    {icon: Icons.skills.Html5, color: "#e6511e", title: "Html5"},
+    {icon: Icons.skills.Css3Alt, color: "#2D53E5", title: "Css3Alt"},
+    {icon: Icons.skills.JsSquare, color: "#f4e224", title: "JsSquare"},
+    {icon: Icons.skills.React, color: "#08d9ff", title: "React"},
+    {icon: Icons.skills.redux, color: "#7a50be", title: "redux"},
+    {icon: Icons.skills.Bootstrap, color: "#7719f7", title: "Bootstrap"},
+    {icon: Icons.skills.Tailwind, color: "#0eb8d5", title: "Tailwind"},
   ],
 };
 export const portfolio = [
@@ -209,7 +213,7 @@ export const portfolio = [
   {
     image: Images.recipeFood,
     title: "recipe food app",
-    topics: ["react", "tailwind", "api",],
+    topics: ["react", "tailwind", "api"],
     category: "js",
     view: "https://mhmd-yhy-recipe-food.netlify.app",
     code: "https://github.com/mhmd-yhy/recipe-food-app",
@@ -220,9 +224,9 @@ export const contact = {
   desc: "Feel free to reach out to discuss your project ideas,collaborations, or any questions you might have.",
   myEmail: "muhammedyahyazx@gmail.com",
   form: [
-    { title: "your name", type: "text", name: "name" },
-    { title: "email", type: "email", name: "email" },
-    { title: "message", type: "text", name: "message" },
+    {title: "your name", type: "text", name: "name"},
+    {title: "email", type: "email", name: "email"},
+    {title: "message", type: "text", name: "message"},
   ],
   sendIcon: Icons.contact.sendContact,
 };
